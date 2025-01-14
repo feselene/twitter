09001c4ccd928ff88acc6c5bb868e27a6480e8e0
feselene <vaporflyb@gmail.com>
2025-01-13 19:46:56 -0500

    add login route to backend

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,36 @@
 const pool = require('../config/db');
 const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    // Validate input
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Email and Password are required.' });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found.' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password.' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+        });
+
+        res.status(200).json({message: 'Login successful', token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
 
 const signupUser = async (req, res) => {
   const { email, password } = req.body;
@@ -34,4 +65,4 @@ const signupUser = async (req, res) => {
   }
 };
 
-module.exports = { signupUser };
+module.exports = { signupUser, loginUser };
