ca109e55aa58a00443267853bd954d6c18958b56
feselene <vaporflyb@gmail.com>
2025-01-18 17:57:38 -0500

    add profilescreen.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import SignupScreen from './src/screens/SignupScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
 
 const Stack = createStackNavigator();
 
